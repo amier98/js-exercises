b936fe4e8489dad44fb2fd0ddcb958bd2d365ae6
Amier Ali <amier_1998@hotmail.com>
2023-10-11 20:00:56 +0100

    feat: Implement all functions

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,24 +1,61 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	
+	const filling = sandwich.fillings
+	return filling
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	const city = person.city
+	if (city == "Manchester") {
+		return true
+	} else {
+		return false
+	}
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	const buses = Math.ceil(people/40)
+	if (people <= 40) {
+		return 1
+	} else if (people <= 80 ) {
+		return 2
+	} else if (people <= 120 ) {
+		return 3
+	} else {
+		return buses
+	}
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
-}
 
+	const newArray = []
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == "sheep") {
+			newArray.push(arr[i])
+		}
+	}
+	
+	return newArray.length
+
+}
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+
+	let isPostcode = true
+	const city = person.address.city
+
+	if (!city.includes("M")) {
+		isPostcode = false
+	} 
+	if (city.includes("M") && !city.includes("anchester")) {
+		isPostcode = false
+	}
+
+	return isPostcode
+
 }
