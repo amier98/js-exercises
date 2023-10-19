9912d2e1065cc8e759738c5507ad4a91c61c4643
Amier Ali <amier_1998@hotmail.com>
2023-10-19 17:16:47 +0100

    feat: Implement change for getFillings function

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,61 +1,58 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	
-	const filling = sandwich.fillings
-	return filling
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  //removed unnecessary variable
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	const city = person.city
-	if (city == "Manchester") {
-		return true
-	} else {
-		return false
-	}
+  if (person === undefined) throw new Error("person is required");
+  const city = person.city;
+  if (city == "Manchester") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	const buses = Math.ceil(people/40)
-	if (people <= 40) {
-		return 1
-	} else if (people <= 80 ) {
-		return 2
-	} else if (people <= 120 ) {
-		return 3
-	} else {
-		return buses
-	}
+  if (people === undefined) throw new Error("people is required");
+  const buses = Math.ceil(people / 40);
+  if (people <= 40) {
+    return 1;
+  } else if (people <= 80) {
+    return 2;
+  } else if (people <= 120) {
+    return 3;
+  } else {
+    return buses;
+  }
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
+  if (arr === undefined) throw new Error("arr is required");
 
-	const newArray = []
+  const newArray = [];
 
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] == "sheep") {
-			newArray.push(arr[i])
-		}
-	}
-	
-	return newArray.length
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "sheep") {
+      newArray.push(arr[i]);
+    }
+  }
 
+  return newArray.length;
 }
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
+  if (person === undefined) throw new Error("person is required");
 
-	let isPostcode = true
-	const city = person.address.city
+  let isPostcode = true;
+  const city = person.address.city;
 
-	if (!city.includes("M")) {
-		isPostcode = false
-	} 
-	if (city.includes("M") && !city.includes("anchester")) {
-		isPostcode = false
-	}
+  if (!city.includes("M")) {
+    isPostcode = false;
+  }
+  if (city.includes("M") && !city.includes("anchester")) {
+    isPostcode = false;
+  }
 
-	return isPostcode
-
+  return isPostcode;
 }
